70f7e76187ff085543aa59492281147d0189d329
Eric <erics1337@gmail.com>
2022-01-26 22:22:38 -0700

    Bold New Message Font

--- client/src/components/Sidebar/ChatContent.js
+++ client/src/components/Sidebar/ChatContent.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Box, Badge, Typography } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
-import { connect } from 'react-redux';
+import { connect } from "react-redux"
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,36 +23,46 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: "auto",
     marginRight: 30,
   },
+	bold: {
+		fontWeight: 900,
+		fontSize: 13,
+	},
 }))
 
 const ChatContent = (props) => {
-  const classes = useStyles()
+	const { conversation, activeConversation } = props
+	const classes = useStyles(conversation)
+	const { latestMessageText, otherUser } = conversation
 
-  const { conversation, activeConversation, user } = props
-  const { latestMessageText, otherUser } = conversation
-
-  return (
-    <Box className={classes.root}>
-      <Box>
-        <Typography className={classes.username}>
-          {otherUser.username}
-        </Typography>
-        <Typography className={classes.previewText}>
-          {latestMessageText}
-        </Typography>
-      </Box>
-      <Box className={classes.badgeBox}>
-        <Badge
-          badgeContent={
-            activeConversation !== otherUser.username ?
-            conversation.messages.filter((msg) => !msg.readByRecipient && msg.senderId !== user.id)
-              .length : 0
-          }
-          color='primary'
-        />
-      </Box>
-    </Box>
-  )
+	return (
+		<Box className={classes.root}>
+			<Box>
+				<Typography className={classes.username}>
+					{otherUser.username}
+				</Typography>
+				<Typography
+					className={
+						activeConversation !== otherUser.username &&
+						conversation.unreadCount > 0
+							? classes.bold
+							: classes.previewText
+					}
+				>
+					{latestMessageText}
+				</Typography>
+			</Box>
+			<Box className={classes.badgeBox}>
+				<Badge
+					badgeContent={
+						activeConversation !== otherUser.username
+							? conversation.unreadCount
+							: 0
+					}
+					color='primary'
+				/>
+			</Box>
+		</Box>
+	)
 }
 
 const mapStateToProps = (state) => {
