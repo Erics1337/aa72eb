ed0b17923743c8fef6239b911877f73df82f10ba
Eric <erics1337@gmail.com>
2022-01-31 15:05:18 -0700

    Circular Import

--- server/messenger_backend/models/conversation.py
+++ server/messenger_backend/models/conversation.py
@@ -2,13 +2,8 @@ from django.db import models
 from django.db.models import Q
 
 from . import utils
-from .user import User
-from .participants import Participants
 
 
 class Conversation(utils.CustomModel):
-    participants = models.ForeignKey(
-        Participants, on_delete=models.CASCADE, db_column="userId", related_name="+"
-    )
     createdAt = models.DateTimeField(auto_now_add=True, db_index=True)
     updatedAt = models.DateTimeField(auto_now=True)
